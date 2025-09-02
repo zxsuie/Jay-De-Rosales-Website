5cd2ee9816294f557bcbae697713979cfd533ea7
Mark Josh Sabeniano <markjosh112345@gmail.com>
2025-09-02 21:20:06 +0000

    change the font of the heading to "Sora"

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,7 +24,7 @@ export default function RootLayout({
           crossOrigin="anonymous"
         />
         <link
-          href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;700&family=Playfair+Display:wght@700&display=swap"
+          href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;700&family=Sora:wght@700&display=swap"
           rel="stylesheet"
         />
       </head>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -11,7 +11,7 @@ export default {
     extend: {
       fontFamily: {
         body: ['Inter', 'sans-serif'],
-        headline: ['Playfair Display', 'serif'],
+        headline: ['Sora', 'sans-serif'],
         code: ['monospace'],
       },
       colors: {
